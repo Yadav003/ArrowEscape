6452339127a339364d2a8ae03ed8c2fbe6c8271c
vikas <vikask54522@gmail.com>
2026-09-05 22:44:20 +0530

    chore: comment out entire contents of server.js

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
 
-const PORT = 3000;
-const MIME_TYPES = {
-  ".html": "text/html; charset=utf-8",
-  ".css": "text/css; charset=utf-8",
-  ".js": "application/javascript; charset=utf-8",
-  ".json": "application/json; charset=utf-8",
-  ".svg": "image/svg+xml; charset=utf-8",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".webp": "image/webp",
-  ".ico": "image/x-icon",
-};
+// const PORT = 3000;
+// const MIME_TYPES = {
+//   ".html": "text/html; charset=utf-8",
+//   ".css": "text/css; charset=utf-8",
+//   ".js": "application/javascript; charset=utf-8",
+//   ".json": "application/json; charset=utf-8",
+//   ".svg": "image/svg+xml; charset=utf-8",
+//   ".png": "image/png",
+//   ".jpg": "image/jpeg",
+//   ".webp": "image/webp",
+//   ".ico": "image/x-icon",
+// };
 
-const server = http.createServer((req, res) => {
-  let reqPath = decodeURI(req.url.split("?")[0]);
-  if (reqPath === "/" || reqPath === "") {
-    reqPath = "/index.html";
-  }
+// const server = http.createServer((req, res) => {
+//   let reqPath = decodeURI(req.url.split("?")[0]);
+//   if (reqPath === "/" || reqPath === "") {
+//     reqPath = "/index.html";
+//   }
 
-  const filePath = path.join(__dirname, reqPath);
-  const ext = path.extname(filePath).toLowerCase();
-  const contentType = MIME_TYPES[ext] || "application/octet-stream";
+//   const filePath = path.join(__dirname, reqPath);
+//   const ext = path.extname(filePath).toLowerCase();
+//   const contentType = MIME_TYPES[ext] || "application/octet-stream";
 
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404 Not Found");
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${err.code}`);
-      }
-    } else {
-      res.writeHead(200, {
-        "Content-Type": contentType,
-        "Access-Control-Allow-Origin": "*",
-      });
-      res.end(content);
-    }
-  });
-});
+//   fs.readFile(filePath, (err, content) => {
+//     if (err) {
+//       if (err.code === "ENOENT") {
+//         res.writeHead(404, { "Content-Type": "text/plain" });
+//         res.end("404 Not Found");
+//       } else {
+//         res.writeHead(500);
+//         res.end(`Server Error: ${err.code}`);
+//       }
+//     } else {
+//       res.writeHead(200, {
+//         "Content-Type": contentType,
+//         "Access-Control-Allow-Origin": "*",
+//       });
+//       res.end(content);
+//     }
+//   });
+// });
 
-server.listen(PORT, () => {
-  console.log(`Arrow Escape server running at http://localhost:${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Arrow Escape server running at http://localhost:${PORT}`);
+// }); 
